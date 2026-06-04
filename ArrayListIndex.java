ab5a0d91ebf3256c9f66a1d66e02be3f3f9a3f1b
Pavitra <pavitrajayamoorthyv@gmail.com>
2026-06-04 10:29:57 +0530

    Add files via upload

---
+++ ArrayListIndex.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+class ArrayListIndex{
+public static void main(String[] args){
+ArrayList<String> list = new ArrayList<>();
+list.add("python");
+list.add("java");
+list.add("c");
+System.out.println(list.indexOf("python"));
+}
+}
