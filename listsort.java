c0cc3055d3c8203c630ee4aab63db081e9623fb0
Pavitra <pavitrajayamoorthyv@gmail.com>
2026-06-04 10:58:33 +0530

    Add files via upload

---
+++ listsort.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class listsort
+{
+public static void main(String[] args)
+{
+ArrayList<Integer> list = new ArrayList<>();
+list.add(40);
+list.add(10);
+list.add(30);
+Collections.sort(list);
+System.out.println(list);
+}
+}
