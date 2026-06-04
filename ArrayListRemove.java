303d5cd81efe248d90472b4a55f2daf3f27e43f1
Pavitra <pavitrajayamoorthyv@gmail.com>
2026-06-04 10:24:45 +0530

    Add files via upload

---
+++ ArrayListRemove.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+class ArrayListRemove{
+public static void main(String[] args){
+ArrayList<String> list = new ArrayList<>();
+list.add("python");
+list.add("java");
+list.add("c");
+list.remove(0);
+System.out.println(list);
+}
+}
---
+++ ArrayListSearch.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+class ArrayListSearch{
+public static void main(String[] args){
+ArrayList<String> list = new ArrayList<>();
+list.add("python");
+list.add("java");
+list.add("c");
+System.out.println(list.contains("java"));
+}
+}
