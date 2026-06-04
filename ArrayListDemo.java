e6493ccd959427b18c366c9ca9df8c50347dde19
Pavitra <pavitrajayamoorthyv@gmail.com>
2026-06-04 09:44:45 +0530

    Add files via upload

---
+++ ArrayListDemo.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+class ArrayListDemo{
+public static void main(String[] args){
+ArrayList<String> fruits = new ArrayList<>();
+fruits.add("apple");
+fruits.add("banana");
+fruits.add("mango");
+System.out.println(fruits);
+}
+}
